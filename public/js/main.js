1eaae7e914625031f79564055c87f62c547fb29f
Kenneth Mead <meadk1@uci.edu>
2019-05-18 16:10:24 -0700

    Update error in main.js

--- public/js/main.js
+++ public/js/main.js
@@ -14,7 +14,7 @@ function getURLParameters(whichParam)
 	}
 }
 
-var.username = getURLParameters('username');
+var username = getURLParameters('username');
 if ('undefined' == typeof username || !username) {
 	username = 'Anonymous_'+Math.random();
 }
