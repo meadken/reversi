42a8972dce4d549d08802e1af13631890f756fdc
Kenneth Mead <meadk1@uci.edu>
2019-05-18 15:52:18 -0700

    add lobby and main js

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+/* functions for general use */
+
+/* This function returns the value associated with 'whichParam' on the URL */
+
+function getURLParameters(whichParam) 
+{
+	var pageURL = window.location.search.substring(1);
+	var pageURLVariables = pageURL.split('&');
+	for (var i = 0; i < pageURLVariables.length; i++) {
+		var parameterName = pageURLVariables[i].split('=');
+		if (parameterName[0] == whichParam) {
+			return parameterName[1];
+		}
+	}
+}
+
+$('#messages').append('<h4>'+getURLParameters('username')+'</h4>');
